64d119954956575c5c0ff603805379b9606405df
suhann-1 <suhanns2001@gmail.com>
2024-06-12 23:26:45 +0530

    add2

---
+++ src/components/View.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const View = () => {
+  return (
+    <div>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">BusNo</th>
+      <th scope="col">StartingFrom</th>
+      <th scope="col">GoingTo</th>
+      <th scope="col">Date</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    
+  </tbody>
+</table>
+    </div>
+  )
+}
+
+export default View
